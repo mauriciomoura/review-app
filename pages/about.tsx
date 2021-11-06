d9e3ab1c90a775731dadbd0b72a55c8f0376f404
Mauricio Moura <mauricio@moura.eti.br>
2021-11-06 09:50:44 -0300

    Adding photos from instagram feed to about page.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,23 @@
-export default function About() {
-    return <h1>This is an about page</h1>
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import React from 'react'
+import InstagramFeed  from 'react-ig-feed'
+import 'react-ig-feed/dist/index.css'
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  
+
+  return {
+    props: {}
+  };
+}
+
+export default function About({
+
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+    return (
+      <InstagramFeed 
+      token="IGQVJYX0U4aE1RUTVzX3Vvd0hCOXpuSUhlWHJ3S3FEcUJzNUs4VlNvYWxmbldVLXA0OGJrQU9OWDFKLVRXZAXp1RW5CUkpkbTRrVHpISFNoS21jUl9WNWQtM09QRkdNaUVqS0JjcTlaR1ZAaMm5lYUlKdwZDZD"  
+      counter="12"/>  
+    );
 }
